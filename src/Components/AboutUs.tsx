dd7819bfc0d4e1d14bf146ef03fd397a24f99c9b
Pramod Pant <pramodpant100@gmail.com>
2022-01-31 08:50:55 +0530

    modal for youtube video added

--- src/Components/AboutUs.tsx
+++ src/Components/AboutUs.tsx
@@ -4,6 +4,7 @@ import illustration from '../Images/illustration.jpg';
 import pro from '../Images/pro.png';
 import reviews from '../Images/reviews.png'
 import './AboutUs.css';
+import YoutubePlayer from './YoutubePlayer';
 import './YoutubePlayer.css';
 
 const AboutUs = () => {
@@ -88,11 +89,7 @@ const AboutUs = () => {
                 
             <div className='video__section'>
             { showModal &&
-            <div className='youtubeplayer'>
-            <svg onClick={()=>setShowModal(false)} aria-hidden="true" focusable="false" data-prefix="fas" data-icon="xmark" className="closeico svg-inline--fa fa-xmark" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 320 512"><path fill="currentColor" d="M310.6 361.4c12.5 12.5 12.5 32.75 0 45.25C304.4 412.9 296.2 416 288 416s-16.38-3.125-22.62-9.375L160 301.3L54.63 406.6C48.38 412.9 40.19 416 32 416S15.63 412.9 9.375 406.6c-12.5-12.5-12.5-32.75 0-45.25l105.4-105.4L9.375 150.6c-12.5-12.5-12.5-32.75 0-45.25s32.75-12.5 45.25 0L160 210.8l105.4-105.4c12.5-12.5 32.75-12.5 45.25 0s12.5 32.75 0 45.25l-105.4 105.4L310.6 361.4z"></path></svg>
-          
-           <iframe id="youtube-7739" frameBorder="0"    width="960" height="540" src="https://www.youtube-nocookie.com/embed/KI2lsdXJQ40?autoplay=1&mute=1"></iframe>
-            </div>
+            <YoutubePlayer OnClose={()=>setShowModal(false)}/>
 
             }          
               <div className='video__box1'>
